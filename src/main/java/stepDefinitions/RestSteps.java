1e6c3fff2a493c2495a60ade1450a4dbae83bdf6
warante <dominguezcalvo.david@gmail.com>
2022-08-16 12:45:07 +0200

    añadidas pruebas de api con rest assured

---
+++ src/main/java/stepDefinitions/RestSteps.java
@@ -0,0 +1,50 @@
+package stepDefinitions;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RestSteps {
+    private Response response;
+
+    @Given("user set api URI {string}")
+    public void userSetApiURI(String uri) {
+        RestAssured.baseURI = uri;
+    }
+
+    @When("user set path {string} service and page {string}")
+    public void user_set_path_service_and_page(String path, String page) {
+         response = given().param("page", page)
+                .get(path);
+    }
+
+    @Then("user expects response code {string}")
+    public void user_expects_response_code(String code) {
+        response.then()
+                .statusCode(Integer.parseInt(code));
+    }
+
+    @When("user set path {string} episode number {string}")
+    public void userSetPathEpisodeNumber(String path, String number) {
+        response = given()
+                .get(path + '/' +number);
+    }
+
+    @And("episode name {string}")
+    public void episodeName(String name) {
+        response.then()
+                .body("name", equalTo(name));
+    }
+
+    @And("episode season {string}")
+    public void episodeSeason(String season) {
+        response.then()
+                .body("episode", equalTo(season));
+    }
+}
---
+++ src/test/features/apiTest.feature
@@ -0,0 +1,25 @@
+Feature: Ricky and Morty, api tests
+
+  @high @get
+  Scenario Outline: Test correctly method get for episodes
+    Given user set api URI "<uri>"
+    When user set path "<path>" service and page "<page>"
+    Then user expects response code "<code>"
+
+    Examples:
+      | uri                               | path      | page  | code  |
+      | https://rickandmortyapi.com/api   | /episode  | 2     | 200   |
+      | https://rickandmortyapi.com/ap    | /episode  | 1     | 404   |
+
+  @high @get
+  Scenario Outline: Test episode name
+    Given user set api URI "<uri>"
+    When user set path "<path>" episode number "<episode>"
+    Then user expects response code "<code>"
+    And episode name "<name>"
+    And episode season "<season>"
+
+    Examples:
+      | uri                               | path      | episode | code  | name                    | season  |
+      | https://rickandmortyapi.com/api   | /episode  | 17      | 200   | The Ricks Must Be Crazy | S02E06  |
+      | https://rickandmortyapi.com/api   | /episode  | 23      | 200   | Rickmancing the Stone   | S03E02  |
